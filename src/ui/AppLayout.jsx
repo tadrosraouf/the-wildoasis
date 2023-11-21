dae98bcd540dfd17c9fd415b9367aea6ad233ab3
tadrosraouf <tadrosraouf95@gmail.com>
2023-11-21 14:50:09 +0200

    starting

---
+++ src/ui/AppLayout.jsx
@@ -0,0 +1,27 @@
+import { Outlet } from "react-router-dom";
+import Sidebar from "./Sidebar";
+import Header from "./Header";
+import styled from "styled-components";
+const Main = styled.main`
+  background-color: var(--color-grey-50);
+  padding: 4rem 4.8rem 6.4rem;
+`;
+const StyledApplayout = styled.div`
+  display: grid;
+  height: 100vh;
+  grid-template-columns: 26rem 1fr;
+  grid-template-rows: auto 1fr;
+`;
+function AppLayout() {
+  return (
+    <StyledApplayout>
+      <Header />
+      <Sidebar />
+      <Main>
+        <Outlet />
+      </Main>
+    </StyledApplayout>
+  );
+}
+
+export default AppLayout;
